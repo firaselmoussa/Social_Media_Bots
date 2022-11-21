269def35b4f08afbd4f4a2805abe9f4b5ecfabe6
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-21 23:58:51 +0200

    updated getImage

--- getImage.py
+++ getImage.py
@@ -1,16 +1,17 @@
-import json
+
 import requests
-from PIL import Image
 
 # https://random.imagecdn.app/500/150
 # https://picsum.photos/200/300
 
 
 def getImage():
-    url = "https://picsum.photos/200/300"
+    url = "https://picsum.photos/200/300?blur=2"
 
     response = requests.get(url)
-    filename = 'fetched_imgs/random_img.jpg'
+
+    filename = "fetched_imgs/random_img.jpg"
+
     with open(filename, 'wb') as f:
         f.write(response.content)
 
