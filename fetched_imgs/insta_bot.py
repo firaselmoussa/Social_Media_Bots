a68e2283397c65ff7f9e3be7d8378240f7284a38
Phoenix-Firas <106271549+Phoenix-Firas@users.noreply.github.com>
2022-11-22 12:12:11 +0200

    creaded bot & logged in

--- fetched_imgs/insta_bot.py
+++ fetched_imgs/insta_bot.py
@@ -1,1 +1,11 @@
 from instabot import Bot
+from create_quote import generateQuotedImage
+import os
+
+# login credentials
+MY_USERNAME = os.environ["WISEBOT_INSTA_USERNAME"]
+MY_PASSWORD = os.environ["WISEBOT_INSTA_PASSWORD"]
+
+# created bot & logged it in
+bot = Bot()
+bot.login(username=MY_USERNAME, password=MY_PASSWORD)
